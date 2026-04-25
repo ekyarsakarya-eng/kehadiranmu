5d5ade2c031e4d40f894af29a050810d8ae32a5e
ekyarsakarya-eng <eky.arsakarya@gmail.com>
2026-04-26 03:46:49 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html", "./*.js"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'header': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'primary': '#800000',
+      }
+    }
+  }
+}
